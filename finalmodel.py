01044e7657bec6152f62b8fde83891a4655ea9b0
L0renz00 <cornelius.lechner@gmail.com>
2023-03-02 10:47:45 +0100

    finales modell walzendrehung und double stepping funktioniert

--- finalmodel.py
+++ finalmodel.py
@@ -2,11 +2,11 @@ from copy import deepcopy
 
 def main():
     #original walzenverdrahtungen der Enigma-I
-    walze_I =     Walze("EKMFLGDQVZNTOWYHXUSPAIBRCJ")
-    walze_II =    Walze("AJDKSIRUXBLHWTMCQGZNPYFVOE")
-    walze_III =   Walze("BDFHJLCPRTXVZNYEIWGAKMUSQO")
-    walze_IV =    Walze("ESOVPZJAYQUIRHXLNFTGKDCMWB")
-    walze_V =     Walze("VZBRGITYUPSDNHLXAWMJQOFECK")
+    walze_I =     Walze("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
+    walze_II =    Walze("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
+    walze_III =   Walze("BDFHJLCPRTXVZNYEIWGAKMUSQO", "D")
+    walze_IV =    Walze("ESOVPZJAYQUIRHXLNFTGKDCMWB", "R")
+    walze_V =     Walze("VZBRGITYUPSDNHLXAWMJQOFECK", "H")
     ukw_b =     ["AY", "BR", "CU", "DH", "EQ", "FS", "GL", "IP", "JX", "KN", "MO", "ZT", "WV"]
 
     #ausgewählte Walzen hier auswählen, 
@@ -17,14 +17,29 @@ def main():
     #umkehrwalze hier auswählen:
     umkehrwalze = deepcopy(ukw_b)
 
+
     enigma = EnigmaKonf(walzen, steckerbrett, umkehrwalze)
-    char = input("Charakter zum Verschlüsseln: ")
 
-    chffr_char = verschluesseln(char, enigma)
+    #test für walzenstellungen nach drehung:
+    for i in (["A"] * (26*26)):
+        verschluesseln(i, enigma)
+        print(f"{enigma.walzen[0].verdrahtung_vor[0]}{enigma.walzen[1].verdrahtung_vor[0]}{enigma.walzen[2].verdrahtung_vor[0]}")
 
-    print("\nVerschluesselter Charakter: ", chffr_char)
 
 def verschluesseln(char, enigma):
+    #vor dem verschluesseln die walzen drehen:
+    #doppelte umdrehung:
+    #wenn mittlere walze umkehr erreicht dann wird rechte gedreht, allerdings 
+    #auch die mittlere, so-gennantes "double stepping"
+    if enigma.walzen[1].verdrahtung_vor[0] == enigma.walzen[1].umkehrchar:
+        enigma.walzen[1].drehen()
+        enigma.walzen[0].drehen()
+    #wenn rechte walze umkehr charakter erreicht wird walze links davon gedreht
+    if enigma.walzen[2].verdrahtung_vor[0] == enigma.walzen[2].umkehrchar:
+        enigma.walzen[1].drehen()
+
+    enigma.walzen[2].drehen()
+
     #verschluesseln:
 
     #steckerbrett:
@@ -48,8 +63,9 @@ def verschluesseln(char, enigma):
 
     return char
 
+
 def zahl_char(zahl):
-    alphabet =    list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")  
+    alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")  
     return alphabet[zahl]
 
 def char_zahl(char):
@@ -67,8 +83,9 @@ class EnigmaKonf:
         self.steckerbrett = Substitution(steckerbrett)
         self.umkehrwalze = Substitution(umkehrwalze)
 
+
 class Walze:
-    def __init__(self, schema):
+    def __init__(self, schema, umkehrchar):
         self.verdrahtung_vor = list(schema)
         #die walzenkonfiguration muss komplett umgekehrt werden 
         #wenn der Strom rückwärts durch die Maschine läuft
@@ -78,7 +95,23 @@ class Walze:
         for i in self.verdrahtung_vor:
             self.verdrahtung_rueck[char_zahl(i)] = zahl_char(self.verdrahtung_vor.index(i))
 
-        #hier kommen später noch die optionen für die walzenrotation nach tastaturdruck rein
+        #charakter der angiebt wann die walze die nächste walze umdreht
+        self.umkehrchar = umkehrchar
+        #counter für rotationsschritte der walze
+        self.umdrehungen = 0
+
+    def drehen(self):
+        #enimga walzen drehen sich so, dass eine umdrehung einer permutation vom ersten charkter zum zweiten entspricht
+        #auch wichtig ist dass erst die drehung geschieht, und dann die Verschlüsselung
+        self.verdrahtung_vor.append(self.verdrahtung_vor[0])
+        self.verdrahtung_vor.pop(0)
+        self.umdrehungen += 1
+
+        #update rueck verschluesselung
+        for i in range(26):
+            self.verdrahtung_rueck.append("")
+        for i in self.verdrahtung_vor:
+            self.verdrahtung_rueck[char_zahl(i)] = zahl_char(self.verdrahtung_vor.index(i))
 
 class Substitution:
     def __init__(self, paarliste):
